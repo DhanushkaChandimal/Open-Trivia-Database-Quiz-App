5a3ada9060c77c7f50c8d896f9a93d1fdcc69524
Dhanushka <dhanushkamcr@gmail.com>
2025-08-05 15:09:07 -0500

    Import bootstrap, create HomePage element and set route

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { Routes, Route } from "react-router-dom"
+import HomePage from "./components/HomePage"
 
 function App() {
 
   return (
     <>
+      <Routes>
+        <Route path = "/" element = {<HomePage/>}/>
+      </Routes>
     </>
   )
 }
